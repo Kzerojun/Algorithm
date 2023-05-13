2e7dad3c6489af9f8c067f36eacb0f3b2075d012
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-05-13 20:12:45 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+    }
+}
