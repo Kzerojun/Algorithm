dd10bfc7119355aa85fc918e34b8b38bed96f213
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-06-02 18:33:39 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 152 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken(), B = st.nextToken();
+        A =A.replace("6","5");
+        B = B.replace("6","5");
+        int min = Integer.parseInt(A)+Integer.parseInt(B);
+
+        A= A.replace("5","6");
+        B= B.replace("5","6");
+        int max = Integer.parseInt(A)+Integer.parseInt(B);
+        System.out.println(min + " " + max);
+
+
+    }
+}
