29ea66c5b8a53921e8ed0fc423c9ee6df00a9c3a
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-09-14 22:19:31 +0900

    [Silver I] Title: 안전 영역, Time: 300 ms, Memory: 55808 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,73 +1,70 @@
 import java.io.*;
 import java.util.*;
 
+class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
     static int N;
-    static int[][] heights;
+    static int[][] heightMap;
     static boolean[][] visited;
+    static int max = -1; // 높이의 최소값으로 초기화
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        heights = new int[N][N];
-        visited = new boolean[N][N];
+        heightMap = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                heights[i][j] = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                max = Math.max(max, num);
+                heightMap[i][j] = num;
             }
         }
 
-        int maxSafeArea = 0;
-        for (int i = 0; i <= 100; i++) {
-            // 각 높이별로 BFS를 수행하여 안전한 영역의 개수를 구함
-            int safeArea = getSafeArea(i);
-            maxSafeArea = Math.max(maxSafeArea, safeArea);
-        }
-
-        System.out.println(maxSafeArea);
-    }
-
-    // BFS로 안전한 영역 개수 계산
-    static int getSafeArea(int heightLimit) {
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        int safeArea = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && heights[i][j] > heightLimit) {
-                    bfs(i, j, heightLimit, dx, dy);
-                    safeArea++;
+        for (int h = 0; h <= max; h++) {
+            visited = new boolean[N][N];
+            int safeAreas = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && heightMap[i][j] > h) {
+                        bfs(i, j, h);
+                        safeAreas++;
+                    }
                 }
             }
+            result = Math.max(result, safeAreas);
         }
 
-        // visited 배열 초기화
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(visited[i], false);
-        }
-
-        return safeArea;
+        System.out.println(result);
     }
 
-    static void bfs(int x, int y, int heightLimit, int[] dx, int[] dy) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
+    static void bfs(int x, int y, int h) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(x, y));
         visited[x][y] = true;
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+            Node tmp = queue.poll();
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = current[0] + dx[dir];
-                int ny = current[1] + dy[dir];
+            for (int i = 0; i < 4; i++) {
+                int ddx = tmp.x + dx[i];
+                int ddy = tmp.y + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N &&
-                        !visited[nx][ny] && heights[nx][ny] > heightLimit) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
+                if (ddx >= 0 && ddx < N && ddy >= 0 && ddy < N && !visited[ddx][ddy] && heightMap[ddx][ddy] > h) {
+                    queue.offer(new Node(ddx, ddy));
+                    visited[ddx][ddy] = true;
                 }
             }
         }
