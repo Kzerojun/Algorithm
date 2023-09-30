5944a0059e18fdcb10d307d5667434024127f4b2
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-09-30 22:54:24 +0900

    [Bronze III] Title: 세 수, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+ 
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+ 
+		if (a >= b) {
+			if (a >= c) {
+				// a가 가장 크거나 모두 같을 경우
+				if (b >= c)
+					System.out.println(b);
+				else
+					System.out.println(c);
+			} else
+				System.out.println(a);
+		} else {
+			if (c <= b) { // b가 가장 큰 수일 경우
+				if (c <= a)
+					System.out.println(a);
+				else
+					System.out.println(c);
+			} else
+				System.out.println(b);
+		}
+	}
+}
