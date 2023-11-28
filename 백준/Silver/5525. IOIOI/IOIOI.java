d4263a3a6725c09b5dc5b37289c0a2a660f389e8
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-11-28 18:23:27 +0900

    [Silver I] Title: IOIOI, Time: 288 ms, Memory: 20064 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    static int sol = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringBuilder p = new StringBuilder("IOI");
-        while (--N > 0) {
-            p.append("OI");
-        }
-        String comparison = p.toString();
-        int S = Integer.parseInt(br.readLine());
-        String string = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+        String S = br.readLine();
 
-        for (int i = 0; i <= string.length() - comparison.length(); i++) {
-            check(i, string, comparison);
-        }
-        System.out.println(sol);
-    }
+        int result = 0;
+        int pattern = 0; 
 
-    static void check(int index, String string, String comparison) {
-        boolean check = true;
-        int count = 0;
-        for (int x = index; x < index + comparison.length(); x++) {
-            if (comparison.charAt(count) != string.charAt(x)) {
-                check = false;
-                break;
+        for(int i=0; i<M-2; i++) {
+            if(S.charAt(i) == 'I' && S.charAt(i+1) == 'O' && S.charAt(i+2) == 'I') {
+                pattern++;
+                if(pattern == N) {
+                    pattern--;
+                    result++;
+                }
+                i++;
+            } else {
+                pattern = 0;
             }
-            count++;
         }
-
-        if (check) {
-            sol++;
-        }
+        System.out.println(result);
     }
 }
