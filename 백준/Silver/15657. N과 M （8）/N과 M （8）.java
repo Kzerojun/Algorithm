3d2e3651de3af7434c5ba2eca469148b312ddfe1
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2023-12-21 15:36:57 +0900

    [Silver III] Title: N과 M (8), Time: 312 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        combination(0,0,"");
+
+    }
+
+    static void combination(int index, int count, String string) {
+        if (count == M) {
+            System.out.println(string);
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            combination(i,count+1,string+arr[i]+" ");
+        }
+    }
+
+
+}
+
