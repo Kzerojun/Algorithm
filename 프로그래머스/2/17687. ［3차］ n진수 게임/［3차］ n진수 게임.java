c262334f4baf9b481d71108acdea42bf2dbccd61
Kzerojun <105963431+Kzerojun@users.noreply.github.com>
2024-02-18 17:57:40 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.74 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,15 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        // n 진법, t 미리 구할 숫자의 객수, 게임참여인원 m, 튜브의 순서
         StringBuilder sb = new StringBuilder();
-        List<Character> list = calculate(n, t);
+        List<Character> list = calculate(n, t * m);
 
-        int count = 0;
         for (int i = p - 1; i < list.size(); i += m) {
             sb.append(list.get(i));
-            count++;
-            if(count==t) {
+            if (sb.length() == t) {
                 break;
             }
         }
@@ -18,33 +15,26 @@ class Solution {
         return sb.toString();
     }
 
-    static List<Character> calculate(int n, int t) {
+    static List<Character> calculate(int n, int limit) {
         List<Character> list = new ArrayList<>();
-        for (int i = 0; i < 1000000; i++) {
-            String string = "";
+        int number = 0;
 
-            int number = i;
-            
-            while (true) {
-                int remainder = number % n;
-                String word;
-                if (remainder >= 10) {
-                    word = String.valueOf((char) ('A' + remainder - 10));
-                } else {
-                    word = String.valueOf(remainder);
-                }
+        while (list.size() < limit) {
+            StringBuilder sb = new StringBuilder();
+            int temp = number;
 
-                string = word + string;
-                number = number / n;
+            do {
+                int remainder = temp % n;
+                String word = (remainder >= 10) ? String.valueOf((char) ('A' + remainder - 10)) : String.valueOf(remainder);
+                sb.insert(0, word);
+                temp = temp / n;
+            } while (temp != 0);
 
-                if (number == 0) {
-                    break;
-                }
+            for (int j = 0; j < sb.length(); j++) {
+                list.add(sb.charAt(j));
             }
 
-            for (int j = 0; j < string.length(); j++) {
-                list.add(string.charAt(j));
-            }
+            number++;
         }
         return list;
     }
