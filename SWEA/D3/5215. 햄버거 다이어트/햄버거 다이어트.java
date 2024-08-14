d7842f61d0af8ef1c25b98cf5e6202b68a3abf9c
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-08-14 13:41:38 +0900

    [D3] Title: 햄버거 다이어트, Time: 144 ms, Memory: 24,008 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,60 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Solution {
-    static int maxScore;
+	static BufferedReader br;
+	static int T;
+	static int[][] dp;
+	static List<Food> foods;
+	
+	static int N,L;
+	public static void main(String[] args) throws IOException {
+		init();
+		play();
+		
+	}
+	
+	private static void init() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+	}
+	
+	private static void play() throws IOException {
+		for(int i = 1; i<=T; i++) {
+			initGame();
+			int result = calculate();
+			System.out.println("#"+i+" "+result);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
+		}
+	}
+	
+	private static void initGame() throws IOException {
 
-        for (int i = 1; i <= testCase; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int L = Integer.parseInt(st.nextToken());
-            HamBurger[] hamBurgers = new HamBurger[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		
+		dp = new int[N+1][L+1];
+		foods = new ArrayList<>();
+		
+		for(int i = 0 ; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int score = Integer.parseInt(st.nextToken());
+			int calorie = Integer.parseInt(st.nextToken());
+			
+			foods.add(new Food(score,calorie));
+		}
+		
+	
+	}
+	
+	private static int calculate() {
+		for(int i = 1; i<=N; i++) {
+			Food food = foods.get(i-1);
+			for(int j = 0; j<=L; j++) {
+				if(food.calorie<=j) {
+					dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-food.calorie]+food.score);
+				}else {
+					dp[i][j] = dp[i-1][j];
+				}
+			}
 
-            for (int j = 0; j < hamBurgers.length; j++) {
-                st = new StringTokenizer(br.readLine());
-                int score = Integer.parseInt(st.nextToken());
-                int calorie = Integer.parseInt(st.nextToken());
-                hamBurgers[j] = new HamBurger(score, calorie);
-            }
+		}
+		
+		return dp[N][L];
+	}
+	
 
-            maxScore = 0;
-            dfs(hamBurgers, L, 0, 0, 0);
-
-            System.out.println("#" + i + " " + maxScore);
-        }
-    }
-
-    static void dfs(HamBurger[] hamBurgers, int limitCalorie, int totalScore, int totalCalorie, int index) {
-        if (totalCalorie > limitCalorie) { 
-            return;
-        }
-
-        if(index == hamBurgers.length) {
-        	if(totalScore>maxScore) {
-        		maxScore = totalScore;
-        	}
-        	return;
-        }
-        
-        dfs(hamBurgers,limitCalorie,totalScore+hamBurgers[index].score,totalCalorie+hamBurgers[index].calorie,index+1);
-        dfs(hamBurgers,limitCalorie,totalScore,totalCalorie,index+1);
-
-    }
 }
 
-class HamBurger {
-    int score;
-    int calorie;
-
-    HamBurger(int score, int calorie) {
-        this.score = score;
-        this.calorie = calorie;
-    }
+class Food {
+	int score;
+	int calorie;
+	
+	Food(int score, int calorie) {
+		this.score = score;
+		this.calorie = calorie;
+	}
 }
-
-
-
