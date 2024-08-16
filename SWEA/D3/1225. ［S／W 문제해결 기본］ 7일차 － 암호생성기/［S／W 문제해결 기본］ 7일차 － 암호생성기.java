2bcd368478f205983ac94dfe396b98726c90ab82
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-08-16 11:39:35 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 137 ms, Memory: 26,396 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,44 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-		static int[] discount = {1,2,3,4,5};
-		
-		public static void main(String[]args) throws IOException{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			int T = 10;
-			
-			for(int tc = 1; tc<=10; tc++) {
-				List<Integer> arr = new ArrayList<>();
-				br.readLine();
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				
-				for(int i = 0; i<8; i++) {
-					arr.add(Integer.parseInt(st.nextToken()));
-				}
-				
-				int index = 0;
-				while(true) {
-					if(arr.get(7)==0) break;
-					int num = arr.remove(0)-discount[index%5];
-					if(num<=0) {
-						num =0;
-					}
-					arr.add(num);
-					index++;
-				}
-				
-				System.out.printf("#%d ",tc);
-				for(int x : arr) {
-					System.out.print(x+" ");
-				}
-				System.out.println();
-			}
-			
 
+	private static int T;
+	private static BufferedReader br;
+
+	public static void main(String[] args) throws IOException {
+		init();
+		start();
+	}
+
+	private static void init() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	private static void start() throws IOException{
+		for(int t = 1; t<=10; t++) {
+			br.readLine();
+			Queue<Integer> q = new LinkedList<>();
+			initGame(q);
+			playGame(q);
+			
+			System.out.print("#"+t+" ");
+			
+			while(!q.isEmpty()) {
+				System.out.print(q.poll()+" ");
+			}
+			System.out.println();
 		}
+	}
+
+	private static void initGame(Queue<Integer> q) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < 8; i++) {
+			int number = Integer.parseInt(st.nextToken());
+			q.add(number);
+		}
+	}
+
+	private static void playGame(Queue<Integer> q) {
+		int count = 1;
+		
+		while (true) {
+			int number = q.poll();
+			number = number - count;
+			count++;
+			
+			if(count ==6) {
+				count = 1;
+			}
+			if (number <= 0) {
+				number = 0;
+				q.add(number);
+				break;
+			}
+			q.add(number);
+		}
+	}
+
 }
