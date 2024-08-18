72a767fdb8467349896d0211be38ed316768b867
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-08-19 00:01:16 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1580 ms, Memory: 374608 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -7,85 +7,80 @@ import java.util.StringTokenizer;
 
 class Main {
 
-	static int N, M, K;
+	static BufferedReader br;
+	static int N, M,K;
 	static int[][] graph;
 
+	// 상, 하, 좌, 우
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws IOException {
 		init();
-		move();
+		System.out.println(move());
 	}
 
 	private static void init() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-
 		graph = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
+			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				graph[i][j] = line.charAt(j) - '0';
+				graph[i][j] = str.charAt(j) - '0';
 			}
 		}
 	}
 
-	private static void move() {
-		Queue<Node> q = new LinkedList<>();
+	private static int move() {
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(0, 0, 1, 0));
+
 		boolean[][][] visited = new boolean[N][M][K+1];
-		q.add(new Node(0, 0, K, 1));
-		visited[0][0][K] = true;
+		visited[0][0][0] = true;
 
 		while (!q.isEmpty()) {
-			Node now = q.poll();
+			Point now = q.poll();
 
 			if (now.x == N - 1 && now.y == M - 1) {
-				System.out.println(now.moveCount);
-				return;
+				return now.moveCount;
 			}
 
 			for (int i = 0; i < 4; i++) {
 				int nx = now.x + dx[i];
 				int ny = now.y + dy[i];
 
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-					continue;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+				if (graph[nx][ny] == 1 && now.destory < K && !visited[nx][ny][now.destory+1]) {
+					visited[nx][ny][now.destory+1] = true;
+					q.add(new Point(nx, ny, now.moveCount + 1, now.destory+1));
 				}
 
-				if (graph[nx][ny] == 1 && now.canMoveCount > 0 && !visited[nx][ny][now.canMoveCount - 1]) {
-					visited[nx][ny][now.canMoveCount - 1] = true;
-					q.add(new Node(nx, ny, now.canMoveCount - 1, now.moveCount + 1));
-				}
-
-				if (graph[nx][ny] == 0 && !visited[nx][ny][now.canMoveCount]) {
-					visited[nx][ny][now.canMoveCount] = true;
-					q.add(new Node(nx, ny, now.canMoveCount, now.moveCount + 1));
+				if (graph[nx][ny] == 0 && !visited[nx][ny][now.destory]) {
+					visited[nx][ny][now.destory] = true;
+					q.add(new Point(nx, ny, now.moveCount + 1, now.destory));
 				}
 			}
 		}
 
-		System.out.println(-1);
+		return -1;
 	}
-
 }
 
-class Node {
+class Point {
+	int x, y;
+	int moveCount, destory;
 
-	int x;
-	int y;
-	int canMoveCount;
-	int moveCount;
-
-	Node(int x, int y, int canMoveCount, int moveCount) {
+	Point(int x, int y, int moveCount, int destory) {
 		this.x = x;
 		this.y = y;
-		this.canMoveCount = canMoveCount;
 		this.moveCount = moveCount;
+		this.destory = destory;
 	}
 }
