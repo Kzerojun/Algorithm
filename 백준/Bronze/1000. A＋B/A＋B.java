4ea35c9fb51db7c38a200b00a047e1c733e99744
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-08-19 15:49:09 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
-
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         int num1 = scanner.nextInt();
-         int num2 = scanner.nextInt();
-         System.out.println(num1+num2);
-        }
-    }
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+ 
+		in.close();
+	}
+}
