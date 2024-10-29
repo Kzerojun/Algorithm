1f12b7dc32c15a492cc4db702052770c218aff94
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-29 21:37:39 +0900

    [Bronze IV] Title: 숫자의 합, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,28 +1,27 @@
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+class Main{
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        String S = br.readLine();
-        int sum = 0;
-        for(int i =0; i<S.length(); i++){
-                        sum = sum + Character.getNumericValue(S.charAt(i));
-        }
-        bw.write(sum+"");
+	public static void main(String[] args) throws IOException {
+		simulate();
+	}
 
-        br.close();
-        bw.flush();
-        bw.close();
-    }
+	private static void simulate() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		String number = br.readLine();
+
+		int sum  = 0;
+		for (char ch : number.toCharArray()) {
+			sum = sum + ch -'0';
+		}
+
+		System.out.println(sum);
+	}
 }
