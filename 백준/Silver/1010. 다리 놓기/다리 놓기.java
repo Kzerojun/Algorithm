5881d52fdcce206a25f4023de4cbf39cb01068d7
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2025-01-21 23:58:00 +0900

    [Silver V] Title: 다리 놓기, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,43 +1,45 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	static int[][] dp = new int[30][30];
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int T = in.nextInt();
-		
-		StringBuilder sb = new StringBuilder();
-        
-		for(int i = 0; i < T; i++) {
-			
+    static int[][] dp;
+    static int tc;
+    static int[][] testCases;
 
-			int N = in.nextInt();	// N = r
-			int M = in.nextInt();	// M = n
-						
-			sb.append(combi(M, N)).append('\n');
-		}
-		
-		System.out.println(sb);
-		
-	}
-	
-	static int combi(int n, int r) {
-		
+    public static void main(String[] args) throws IOException {
+        init();
 
-		if(dp[n][r] > 0) {
-			return dp[n][r]; 
-		}
-		
+        for (int i = 0; i < tc; i++) {
+            int n = testCases[i][0]; // 서쪽
+            int m = testCases[i][1]; // 동쪽
 
-		if(n == r || r == 0) {
-			return dp[n][r] = 1;
-		}
-		
+            System.out.println(simulate(m, n));
+        }
+    }
 
-		return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
-	}
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp = new int[30][30];
+
+        tc = Integer.parseInt(br.readLine());
+        testCases = new int[tc][2];
+
+        for (int i = 0; i < tc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            testCases[i][0] = Integer.parseInt(st.nextToken()); // 서쪽
+            testCases[i][1] = Integer.parseInt(st.nextToken()); // 동쪽
+        }
+    }
+
+    static int simulate(int n, int r) {
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = simulate(n - 1, r - 1) + simulate(n - 1, r);
+    }
 }
