7420174be9d7617301f7483dc9c064f3e3df7f89
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2025-05-15 20:48:03 +0900

    [Bronze I] Title: 단어 공부, Time: 352 ms, Memory: 36288 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,35 +1,69 @@
-import java.io.IOException;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int[] alphabet = new int[26];
-    String word = br.readLine().toUpperCase();
+public class Main {
 
-    for(int i  =0; i<word.length(); i++){
-        int num = word.charAt(i) - 'A';
-        alphabet[num]++;
-    }
-    int max = -1;
-    char ch = '?';
-    for( int j =0; j<alphabet.length;j++){
-        if(max < alphabet[j]){
-            max = alphabet[j];
-            ch = (char)(j+'A');
-        }
-        else if (alphabet[j]==max){
-            ch ='?';
-        }
-    }
-    bw.write(ch+"");
-    br.close();
-    bw.flush();
-    bw.close();
+	static Map<Character, Integer> maps;
+	static String word;
+	static int max;
 
-    }
+
+	public static void main(String[] args) throws Exception {
+		init();
+		char result = simulate();
+		printResult(result);
+	}
+
+	private static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		maps = new HashMap<>();
+		word = br.readLine();
+
+		max = Integer.MIN_VALUE;
+	}
+
+	private static char simulate() {
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+
+			char upperCase = Character.toUpperCase(ch);
+			maps.put(upperCase, maps.getOrDefault(upperCase, 0) + 1);
+			max = Math.max(max, maps.get(upperCase));
+		}
+
+		boolean check = false;
+		char result =  ' ';
+		for (char key : maps.keySet()) {
+			int value = maps.get(key);
+
+			if (value == max) {
+				if (check) {
+					result = '?';
+					return result;
+				}
+				result = key;
+				check = true;
+			}
+		}
+
+		return result;
+	}
+
+
+	private static void printResult(char result) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(result + " ");
+		bw.flush();
+		bw.close();
+	}
+
 }
